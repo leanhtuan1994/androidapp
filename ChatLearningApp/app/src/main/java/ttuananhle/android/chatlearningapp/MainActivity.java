2ec4e575488620addfaf6d3aca76a3d65ff2358c
Tuan Anh Le <leanhtuanptithcm@gmail.com>
2017-05-03 19:50:54 +0700

    Create BottomBar for App
    Create BottomBar

--- ChatLearningApp/app/src/main/java/ttuananhle/android/chatlearningapp/MainActivity.java
+++ ChatLearningApp/app/src/main/java/ttuananhle/android/chatlearningapp/MainActivity.java
@@ -1,13 +1,36 @@
 package ttuananhle.android.chatlearningapp;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabReselectListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomBar bottomBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // init bottom bar
+        initBottomBar();
+
+
+    }
+
+    private void initBottomBar(){
+        bottomBar = (BottomBar) this.findViewById(R.id.bottomBar);
+
+        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(@IdRes int tabId) {
+
+            }
+        });
     }
 }
